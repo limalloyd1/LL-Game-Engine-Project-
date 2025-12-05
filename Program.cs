a9108ec7861124f177ee762b46e0c1e68cdfa06a
Liam Lloyd <liamlloyd2025@gmail.com>
2025-12-04 23:19:59 -0600

    Integrate terrain generation into main program
    Added terrain generation functionality and integrated it into the rendering process.

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ using System.Windows;
 using FBXModelLoaderSpace;
 using Assimp;
 using System.IO;
+using TerrainGeneration;
 
 // using PlayerNameSpace;
 // using ApplicationNameSpace;
@@ -18,7 +19,7 @@ namespace mainNameSpace
         {
             Console.WriteLine("Starting main function: ");
             FBXModelLoader modelLoader = new FBXModelLoader();
-            string filePath = @"C:\Users\liamb\OneDrive\Desktop\3DModels\bruh\exports\solarPunkTree.fbx";
+            string filePath = @"C:\Users\liamb\OneDrive\Desktop\3DModels\bruh\exports\solarPunkTV.fbx";
             Console.WriteLine($"Looking for file: {filePath}");
             if (System.IO.File.Exists(filePath))
             {
@@ -53,11 +54,25 @@ namespace mainNameSpace
             Console.WriteLine("Starting OpenGL window...");
             var renderer = new openGLRenderer(1280, 720, "Game Engine Window");
 
+            // Load fbx data
             renderer.LoadMeshData(vertices, indices, normals, uvs);
+
+            // Load Terrain Data 
+            var terrainGen = new TerrainGenerator(500,500, 1.0f); // 50x50 terrain , 1 unit cells
+            terrainGen.GenerateFlatTerrain(out float[] terrainVertices, out uint[] terrainIndices, out float[] terrainNormals);
+            terrainGen.GenerateUVs(out float[] terrainUVs);
+            Console.WriteLine($"Terrain generated: {terrainVertices.Length / 3} verts, {terrainIndices.Length / 3} tris");
+            renderer.LoadTerrainData(terrainVertices,terrainIndices, terrainNormals, terrainUVs);
+
 
             // Compile Shaders and run
             renderer.CompileShaders(openGLRenderer.VertexShaderSource, openGLRenderer.FragmentShaderSource);
+            Console.WriteLine("Shaders Compiled");
             renderer.RotateMesh(0,MathF.PI / 2,0);
+            renderer.SetMeshScale(0.1f);
+
+            renderer.SetMeshColor(1.0f, 0.0f, 0.0f);
+
             renderer.Run();
 
 
@@ -72,6 +87,7 @@ namespace mainNameSpace
             // TODO: Store this data in a structure for rendering later
         }
 
+        // For mesh logging
         static void ProcessModelData(FBXModelLoader loader, Scene scene)
         {
             using (StreamWriter writer = new StreamWriter("debug_output.txt", append: true))
